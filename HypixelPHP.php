b614fdc37716d5bb710b858ac47f526e18e5b139
Plancke <aaronplancker@gmail.com>
2014-10-26 18:03:31 +0100

    yoloswag

---
+++ HypixelPHP.php
@@ -0,0 +1,142 @@
+<?php
+
+/**
+ * HypixelPHP
+ *
+ * @author Plancke @f6design
+ * @version 1.0.0
+ * @link  http://plancke.nl
+ *
+ */
+class HypixelPHP
+{
+    private $options;
+
+    public function  __construct($options = array())
+    {
+
+        $this->options = array_merge(
+            array(
+                'api_key' => ''
+            ),
+            $options
+        );
+    }
+
+    public function get_player($keypair = array())
+    {
+        $pairs = array_merge(
+            array(
+                'name' => '',
+                'uuid' => '',
+                'id' => ''
+            ),
+            $keypair
+        );
+
+        $response = array('succes' => 'false');
+        foreach ($pairs as $key => $val) {
+            if ($val != '') {
+                $response = json_decode(http_get('https://api.hypixel.net/player?key=' . $this->options['api_key'] . '&' . $key . '=' . $val));
+                break;
+            }
+        }
+
+        if ($response['succes'])
+            return new Player($this->options['api_key'], $response);
+
+        return null;
+    }
+
+
+}
+
+class Player
+{
+    private $infojson;
+    private $api_key;
+    private $guild;
+
+    public function __construct($api_key, $json)
+    {
+        $this->infojson = $json;
+        $this->api_key = $api_key;
+        $this->guild = $this->getGuild();
+    }
+
+    public function getAPI()
+    {
+        return $this->api_key;
+    }
+
+    public function getRaw()
+    {
+        return $this->infojson;
+    }
+
+    public function get($key)
+    {
+        return $this->infojson[$key];
+    }
+
+    public function getName()
+    {
+        return $this->get('displayname');
+    }
+
+    public function getUUID()
+    {
+        return $this->get('uuid');
+    }
+
+    public function getRank()
+    {
+        return $this->get('packageRank');
+    }
+
+    public function getStats()
+    {
+        return $this->get('stats');
+    }
+
+    public function getGuild()
+    {
+        if ($this->guild != null)
+            return $this->guild;
+
+        $id = json_decode(http_get('https://api.hypixel.net/findGuild?key=' . $this->api_key . '&byPlayer' . $this->getName()));
+        if ($id['succes']) {
+            $guild = json_decode(http_get('https://api.hypixel.net/guild?key=' . $this->api_key . '&id' . $id['_id']));
+            if ($guild['success'])
+                return new Guild($this->api_key, $guild);
+        }
+        return null;
+    }
+}
+
+class Guild
+{
+    private $infojson;
+    private $api_key;
+
+    public function __construct($api_key, $json)
+    {
+        $this->infojson = $json;
+        $this->api_key = $api_key;
+    }
+
+    public function getAPI()
+    {
+        return $this->api_key;
+    }
+
+    public function getRaw()
+    {
+        return $this->infojson;
+    }
+
+    public function get($key)
+    {
+        return $this->infojson[$key];
+    }
+}
