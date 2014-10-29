172de5a5d8502db4ae6827cc9853bdc573da37ae
Plancke <aaronplancker@gmail.com>
2014-10-29 06:51:25 +0100

    add caching name support

--- test.php
+++ test.php
@@ -5,8 +5,5 @@ ini_set('html_errors', 0);
 error_reporting(-1);
 
 include_once('HypixelPHP.php');
-$HypixelPHP = new HypixelPHP(array('api_key'=>'63ba5ba5-d858-4399-9ff2-82d0322bf6a2'));
+$HypixelPHP = new HypixelPHP(array('api_key'=>'0a724ac9-fa5e-433f-89f5-c0c28605bc5c'));
 $player = $HypixelPHP->getPlayer(array('name'=>'Plancke'));
-print($player->get('displayname'));
-$guild = $player->getPlayerGuild($HypixelPHP);
-print_r($guild->get('members.0.rank'));
