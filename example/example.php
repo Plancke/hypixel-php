87d319c1bdc6d64dc26559f19e2f0af78aea6f8a
Plancke <aaronplancker@gmail.com>
2014-11-04 14:55:30 +0100

    remove old example

--- example/example.php
+++
@@ -1,6 +0,0 @@
-<?php
-$HypixelPHP = new HypixelPHP(array('api_key'=>'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx'));
-$player = $HypixelPHP->getPlayer(array('name'=>'Plancke'));
-echo $player->getName();
-$guild = $HypixelPHP->getGuild(array('byName'=>'PainBall'));
-echo $guild->getTag();
