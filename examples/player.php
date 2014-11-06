644719c446b59b2854e8a4d00b86d6dbb902c57a
Plancke <aaronplancker@gmail.com>
2014-11-06 18:16:17 +0100

    add example for new stats object

--- examples/player.php
+++ examples/player.php
@@ -6,3 +6,17 @@ $HypixelPHP = new HypixelPHP(array('api_key'=>'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxx
 $player = $HypixelPHP->getPlayer(array('name'=>'Plancke'));
 // once you have the player object you can do whatever you want :D
 echo $player->getName();
+
+$allstats = $player->getStats();
+foreach(array_keys($allstats->getRaw()) as $game)
+{
+    echo '<h1>' . $game . '</h1>';
+    echo '<ul>';
+
+    $gamestats = $allstats->getGame($game);
+    foreach($allstats->getGame($game)->getRaw() as $field=>$val)
+    {
+        echo '<li><b>' . $field . ':</b> ' . $val . '</li>';
+    }
+    echo '</ul><br />';
+}
