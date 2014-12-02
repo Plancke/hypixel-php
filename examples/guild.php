7c4fc6dfa6f7f915c92fc523918e7ead40a4f160
Plancke <aaronplancker@gmail.com>
2014-12-02 13:39:05 +0100

    fix Throttle

--- examples/guild.php
+++ examples/guild.php
@@ -11,6 +11,10 @@ foreach($memberlist as $rank=>$members)
     echo "<h1>$rank</h1>";
     echo '<ul>';
 
+    // Don't want to load players here, big guilds
+    // We're only getting the exact name from cache or new if player
+    // doesn't exist yet
+    $HypixelPHP->set(array('cache_time'=>'9999999999999'));
     foreach($members as $member) {
         $player = $HypixelPHP->getPlayer(array('name'=>$member['name']));
         $player_name = $player->getName();
