4dac41d1f19915d13d411a72981d94aec684635d
Plancke <Plancke@users.noreply.github.com>
2014-12-11 19:12:40 +0100

    Update guild.php

--- examples/guild.php
+++ examples/guild.php
@@ -14,7 +14,7 @@ foreach($memberlist as $rank=>$members)
     // Don't want to load players here, big guilds
     // We're only getting the exact name from cache or
     // new if player doesn't exist yet
-    $HypixelPHP->set(array('cache_time'=>'9999999999999'));
+    $HypixelPHP->set(array('cache_time'=>9999999999999));
     foreach($members as $member) {
         $player = $HypixelPHP->getPlayer(array('name'=>$member['name']));
         $player_name = $player->getName();
