c6ddea5d9b73a304e23ffc25dfd0eb2974c4c590
kipcode66 <egamer1010@gmail.com>
2014-12-15 12:08:05 -0500

    Achievements
    I added a beginning of Achievements support. It have to be completed, and I'm working on the rest of that.

--- HypixelPHP.php
+++ HypixelPHP.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * HypixelPHP
  *
@@ -11,7 +10,6 @@
 class HypixelPHP
 {
     private $options;
-
     public function  __construct($input = array())
     {
         $this->options = array_merge(
@@ -29,24 +27,19 @@ class HypixelPHP
             ),
             $input
         );
-
         if(!file_exists($this->options['cache_folder_player'])) {
             mkdir($this->options['cache_folder_player'], 0777, true);
         }
-
         if(!file_exists($this->options['cache_folder_guild'])) {
             mkdir($this->options['cache_folder_guild'], 0777, true);
         }
-
         if(!file_exists($this->options['cache_folder_friends'])) {
             mkdir($this->options['cache_folder_friends'], 0777, true);
         }
-
         if(!file_exists($this->options['cache_folder_sessions'])) {
             mkdir($this->options['cache_folder_sessions'], 0777, true);
         }
     }
-
     public function set($input)
     {
         foreach($input as $key=>$val)
@@ -54,12 +47,10 @@ class HypixelPHP
             $this->options[$key] = $val;
         }
     }
-
     public function getVersion()
     {
         return $this->options['version'];
     }
-
     public function setKey($key)
     {
         $this->options['api_key'] = $key;
@@ -68,18 +59,15 @@ class HypixelPHP
     {
         return $this->options['api_key'];
     }
-
     public function getOptions()
     {
         return $this->options;
     }
-
     public function fetch($request, $key, $val)
     {
         $response = @file_get_contents('https://api.hypixel.net/' . $request . '?key=' . $this->getKey() . '&' . $key . '=' . $val);
         return json_decode($response, true);
     }
-
     public function getPlayer($keypair = array())
     {
         $pairs = array_merge(
@@ -89,14 +77,12 @@ class HypixelPHP
             ),
             $keypair
         );
-
         foreach ($pairs as $key => $val) {
             $val = strtolower($val);
             if ($val != '') {
                 if ($key == 'uuid') {
                     $filename = $this->options['cache_folder_player'] . $this->options['cache_uuid_table'];
                     $content = json_decode($this->getContent($filename), true);
-
                     if(array_key_exists($val, $content))
                     {
                         if(time() - $this->options['cache_time'] < $content[$val]['timestamp'])
@@ -104,19 +90,15 @@ class HypixelPHP
                             return $this->getPlayer(array('name'=>$content[$val]['name']));
                         }
                     }
-
                     $response = $this->fetch('player', $key, $val);
                     if ($response['success']) {
                         $content[$val] = array('timestamp'=>time(), 'name'=>$response['player']['displayname']);
                         $this->setContent($filename, json_encode($content));
                         return new Player($response['player'], $this);
                     }
-
                 }
-
                 if ($key == 'name') {
                     $filename = $this->options['cache_folder_player'] . $key . '/' . $this->getCacheFileName($val) . '.json';
-
                     if (file_exists($filename)) {
                         if (time() - $this->options['cache_time'] < filemtime($filename)) {
                             $content = json_decode($this->getContent($filename), true);
@@ -125,7 +107,6 @@ class HypixelPHP
                     } else {
                         @mkdir(dirname($filename), 0777, true);
                     }
-
                     $response = $this->fetch('player', $key, $val);
                     if ($response['success']) {
                         $this->setContent($filename, json_encode($response['player']));
@@ -146,20 +127,17 @@ class HypixelPHP
             ),
             $keypair
         );
-
         foreach ($pairs as $key => $val) {
             if ($val != '') {
                 $val = str_replace(' ', '%20', $val);
                 if ($key == 'byPlayer' || $key == 'byName') {
                     $filename = $this->options['cache_folder_guild'] . $this->options['cache_' . $key . '_table'];
                     $content = json_decode($this->getContent($filename), true);
-
                     if (array_key_exists($val, $content)) {
                         if (time() - $this->options['cache_time'] < $content[$val]['timestamp']) {
                             return $this->getGuild(array('id' => $content[$val]['guild']));
                         }
                     }
-
                     // new/update entry
                     $response = $this->fetch('findGuild', $key, $val);
                     if ($response['success']) {
@@ -168,7 +146,6 @@ class HypixelPHP
                         return $this->getGuild(array('id' => $response['guild']));
                     }
                 }
-
                 if ($key == 'id') {
                     $filename = $this->options['cache_folder_guild'] . $key . '/' . $val . '.json';
                     if (file_exists($filename)) {
@@ -179,7 +156,6 @@ class HypixelPHP
                     } else {
                         @mkdir(dirname($filename), 0777, true);
                     }
-
                     // new/update entry
                     $response = $this->fetch('guild', $key, $val);
                     if ($response['success']) {
@@ -199,7 +175,6 @@ class HypixelPHP
             ),
             $keypair
         );
-
         foreach ($pairs as $key => $val) {
             $val = strtolower($val);
             if ($val != '') {
@@ -214,7 +189,6 @@ class HypixelPHP
                     } else {
                         @mkdir(dirname($filename), 0777, true);
                     }
-
                     $response = $this->fetch('session', $key, $val);
                     if ($response['success']) {
                         $this->setContent($filename, json_encode($response['session']));
@@ -223,7 +197,7 @@ class HypixelPHP
                 }
             }
         }
-        return new Session(null, $this);;
+        return new Session(null, $this);
     }
     public function getFriends($keypair = array())
     {
@@ -233,7 +207,6 @@ class HypixelPHP
             ),
             $keypair
         );
-
         foreach ($pairs as $key => $val) {
             $val = strtolower($val);
             if ($val != '') {
@@ -248,7 +221,6 @@ class HypixelPHP
                     } else {
                         @mkdir(dirname($filename), 0777, true);
                     }
-
                     $response = $this->fetch('friends', $key, $val);
                     if ($response['success']) {
                         $this->setContent($filename, json_encode($response['records']));
@@ -259,7 +231,6 @@ class HypixelPHP
         }
         return new Friends(null, $this);
     }
-
     private function getContent($filename)
     {
         @mkdir(dirname($filename), 0777, true);
@@ -275,7 +246,6 @@ class HypixelPHP
         }
         return $content;
     }
-
     private function setContent($filename, $content)
     {
         @mkdir(dirname($filename), 0777, true);
@@ -283,7 +253,6 @@ class HypixelPHP
         fwrite($file, $content);
         fclose($file);
     }
-
     private function getCacheFileName($input)
     {
         if(strlen($input) < 3 ) {
@@ -291,28 +260,22 @@ class HypixelPHP
         }
         return substr($input, 0, 1) . '/' . substr($input, 1, 1) . '/' . substr($input, 2);
     }
-
 }
-
 class HypixelObject {
     public $JSONArray;
     public $api;
-
     public function __construct($json, $api)
     {
         $this->JSONArray = $json;
         $this->api = $api;
     }
-
     public function isNull() {
         return $this->getRaw() == null;
     }
-
     public function getRaw()
     {
         return $this->JSONArray;
     }
-
     public function get($key, $implicit = false, $default = null)
     {
         if($this->isNull()) return $default;
@@ -326,30 +289,25 @@ class HypixelObject {
                 } else {
                     return $default;
                 }
-
             }
             return $return ? $return : $default;
         }
         return in_array($key, array_keys($this->JSONArray)) ? $this->JSONArray[$key] : $default;
     }
-
     public function getId()
     {
         return $this->get('_id', true);
     }
 }
-
 class Player extends HypixelObject {
     public function getSession()
     {
         return $this->api->getSession(array('player'=>$this->getName()));
     }
-
     public function getFriends()
     {
         return $this->api->getFriends(array('player'=>$this->getName()));
     }
-
     public function getName()
     {
         if($this->get('displayname', true) != null)
@@ -366,26 +324,21 @@ class Player extends HypixelObject {
             return $aliases[0];
         }
     }
-
     public function getUUID(){
         return $this->get('uuid');
     }
-
     public function getStats()
     {
         return new Stats($this->get('stats', true, array()), $this->api);
     }
-
     public function isPreEULA()
     {
         return $this->get('eulaCoins', true, false);
     }
-
     public function getLevel()
     {
         return $this->get('networkLevel', true, 0) + 1;
     }
-
     public function isStaff()
     {
         $rank = $this->get('rank', true);
@@ -393,7 +346,6 @@ class Player extends HypixelObject {
             return false;
         return true;
     }
-
     public function getMultiplier()
     {
         if($this->getRank(false) == 'YOUTUBER') return 7;
@@ -404,7 +356,6 @@ class Player extends HypixelObject {
         $levelKey = floor($this->getLevel() / 25) + 1;
         return ($rankKey > $levelKey) ? $rankKey : $levelKey;
     }
-
     public function getRank($package = true, $preEULA = false)
     {
         if($package)
@@ -449,26 +400,113 @@ class Player extends HypixelObject {
         }
         return 'DEFAULT';
     }
-
     public function getAchievements() {
-
+        return new Achievements($this->JSONArray["achievementsOneTime"], $this->api, $this);
     }
-
     public function getAchievementPoints() {
         return 'WIP';
     }
 }
-
+class Achievements extends HypixelObject {
+    const GAME_GENERAL = "general";
+    const GAME_QUAKE = "quake";
+    const GAME_PAINTBALL = "paintball";
+    const GAME_WALLS = "walls";
+    const GAME_VAMPIREZ = "vampirez";
+    const GAME_BLITZ = "blitz";
+    const GAME_MEGAWALLS = "walls3";
+    const GAME_ARENA = "arena";
+    
+    const GAME_ARCADE = "arcade";
+    const GAME_ARCADE_CREEPERATTACK = GAME_ARCADE.".creeper";
+    
+    const GAME_TNTGAMES = "tntgames";
+    const GAME_TNTGAMES_WIZARDS = GAME_TNTGAMES.".wizards";
+    const GAME_TNTGAMES_BOW = GAME_TNTGAMES.".bow";
+    const GAME_TNTGAMES_TNT_RUN = GAME_TNTGAMES.".tnt.run";
+    const GAME_TNTGAMES_TNT_TAG = GAME_TNTGAMES.".tnt.tag";
+    
+    
+    private $player;
+    
+    public function __construct($json, $api, $player){
+        parent::__construct($json, $api);
+        $this->player = $player;
+    }
+    public function getByGame($key/* should be the game name */){
+        $achievements = $this->JSONArray;
+        $availableGamesList = array();
+        foreach($achievements as $achievement){
+            $gameName = explode('_',$achievement)[0];
+            if(!in_array($gameName, array_keys($availableGamesList))){
+                $availableGamesList[$gameName] = array();
+            }
+            if(in_array($gameName, array("arcade","tntgames"))){
+                $subGameName = explode('_',$achievement)[1];
+                if(!in_array($subGameName, array_keys($availableGamesList[$gameName]))) $availableGamesList[$gameName][$subGameName] = array();
+                $availableGamesList[$gameName][$subGameName][] = $achievement;
+            }else{
+                $availableGamesList[$gameName][] = $achievement;
+            }
+        }
+        //return $availableGamesList[$key];
+        if(count($availableGamesList) <= 0)
+            return null;
+        $tmpGameList = $availableGamesList;
+        foreach(explode('.', $key) as $subGame){
+            if(in_array($subGame, array_keys($tmpGameList))) $tmpGameList = $tmpGameList[$subGame];
+            else return null;
+        }
+        //if(in_array($key, array_keys($availableGamesList))){
+        $ret = array();
+        foreach($tmpGameList as $game => $achievement){
+            /* if(in_array($key, array("arcade","tntgames"))){
+                $ret[] = array();
+                foreach($achievement as $subGame => $achievement2){
+                    $ret[count($ret)-1][$subGame] = new OneTimeAchievement($achievement2, $this->player);
+                }
+            }else */
+            $ret[] = new OneTimeAchievement($achievement, $this->player);
+        }
+        return $ret;
+        //}else
+        //    return array("none");
+    }
+    
+    public function hasAchievement($ach){
+        return in_array($ach, $this->JSONArray);
+    }
+    
+    public function __toString(){
+        $this->JSONArray->__toString();
+    }
+}
 class OneTimeAchievement {
-
+    
+    private $name;
+    private $player;
+    
+    public function __construct($key = null, $player = null) {
+        $this->name = $key;
+        $this->player = $player;
+    }
+    public function isNull() {
+        return $this->name == null || !is_string($this);
+    }
+    public function getName() {
+        return $this->name;
+    }
+    public function getDescription() {
+        // not done for now
+    }
+    public function __toString() {
+        $this->name;
+    }
 }
 class TieredAchievement {
-
 }
 class Tier {
-
 }
-
 class Stats extends HypixelObject {
     public function getGame($game)
     {
@@ -476,47 +514,37 @@ class Stats extends HypixelObject {
     }
 }
 class GameStats extends HypixelObject { /* Dummy for now */ }
-
 class Session extends HypixelObject {
     public function getPlayers() {
         return $this->get('players', true);
     }
-
     public function getGame() {
         return $this->get('gameType', true);
     }
-
     public function getServer() {
         return $this->get('server', true);
     }
 }
-
 class Friends extends HypixelObject { /* Dummy for now */ }
-
 class Guild extends HypixelObject
 {
     private $members;
-
     public function getName()
     {
         return $this->get('name', true);
     }
-
     public function canTag()
     {
         return $this->get('canTag', true);
     }
-
     public function getTag()
     {
         return $this->get('tag', true);
     }
-
     public function getCoins()
     {
         return $this->get('coins', true);
     }
-
     public function getMemberList()
     {
         if($this->members != null)
@@ -524,25 +552,21 @@ class Guild extends HypixelObject
         $this->members = new MemberList($this->JSONArray['members'], $this->api);
         return $this->getMemberList();
     }
-
     public function getMemberCount()
     {
         if($this->members == null)
-            $this->members = new MemberList($this->JSONArray['members'], $this->api);
+            $this->members = new MemberList($this->JSONArray['members'], $this->vvvvv);
         return $this->members->getMemberCount();
     }
-
     public function getMaxMembers()
     {
         $upgrades = array(0,5,5,5,5,5,5,5,5,10,5,5,5,5,5,5,5,10);
         $base = 25;
-
         $total = 0;
         for($i = 0; $i <= $this->get('memberSizeLevel', true, 0); $i++)
         {
             @$total += $upgrades[$i];
         }
-
         $total += $base;
         return $total;
     }
@@ -550,12 +574,10 @@ class Guild extends HypixelObject
 class MemberList extends HypixelObject {
     private $list;
     private $count;
-
     public function __construct($json, $api)
     {
         $this->JSONArray = $json;
         $this->api = $api;
-
         $list = array("GUILDMASTER"=>array(), "OFFICER"=>array(), "MEMBER"=>array());
         $this->count = sizeof($json);
         foreach($json as $player)
@@ -565,17 +587,14 @@ class MemberList extends HypixelObject {
             {
                 $list[$rank] = array();
             }
-
             array_push($list[$rank], $player);
         }
         $this->list = $list;
     }
-
     public function getList()
     {
         return $this->list;
     }
-
     public function getMemberCount()
     {
         return $this->count;
