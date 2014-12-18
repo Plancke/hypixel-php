f88c0c27a00fbf08dbfe728417b471021025c506
Plancke <aaronplancker@gmail.com>
2014-12-18 06:23:44 +0100

    update examples

--- examples/guild.php
+++ examples/guild.php
@@ -1,26 +1,29 @@
 <?php
-$HypixelPHP = new HypixelPHP(array('api_key'=>'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx'));
-$guild = $HypixelPHP->getGuild(array('byName'=>'PainBall'));
-$memberlist = $guild->getMemberList()->getList();
+$HypixelPHP = new HypixelPHP(array('api_key' => 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx'));
+$guild = $HypixelPHP->getGuild(array('byName' => 'PainBall'));
+if (!$guild->isNull()) {
+    $memberlist = $guild->getMemberList()->getList();
 
-echo 'Guild Name: ' . $guild->getName();
-echo 'Guild Tag: ' . $guild->getTag();
-
-foreach($memberlist as $rank=>$members)
-{
-    echo "<h1>$rank</h1>";
-    echo '<ul>';
-
-    // Don't want to load players here, big guilds
-    // We're only getting the exact name from cache or
-    // new if player doesn't exist yet
-    $HypixelPHP->set(array('cache_time'=>9999999999999));
-    foreach($members as $member) {
-        $player = $HypixelPHP->getPlayer(array('name'=>$member['name']));
-        $player_name = $player->getName();
-
-        echo "<li>$player_name</li>";
+    echo 'Guild Name: ' . $guild->getName();
+    if ($guild->canTag()) {
+        echo 'Guild Tag: ' . $guild->getTag();
     }
 
-    echo '</ul>';
+    foreach ($memberlist as $rank => $members) {
+        echo "<h1>$rank</h1>";
+
+        // Don't want to load players here, big guilds would
+        // throttle your key. We're only getting the exact name
+        // from cache or new if player doesn't exist yet
+        echo '<ul>';
+        $HypixelPHP->set(array('cache_time' => $HypixelPHP::MAX_CACHE_TIME));
+        foreach ($members as $member) {
+            $player = $HypixelPHP->getPlayer(array('name' => $member['name']));
+            $player_name = $player->getName();
+
+            echo "<li>$player_name</li>";
+        }
+
+        echo '</ul>';
+    }
 }
--- examples/player.php
+++ examples/player.php
@@ -1,25 +1,26 @@
 <?php
 include_once('HypixelPHP.php');
 // instantiate the hypixel api object using your api key
-$HypixelPHP = new HypixelPHP(array('api_key'=>'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx'));
+$HypixelPHP = new HypixelPHP(array('api_key' => 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx'));
 // get a player object using the hypixel api object
-$player = $HypixelPHP->getPlayer(array('name'=>'Plancke'));
-// once you have the player object you can do whatever you want :D
-echo $player->getName();
+$player = $HypixelPHP->getPlayer(array('name' => 'Plancke'));
+// Check if Object is null
+// All Objects returned by this Wrapper have this function
+if (!$player->isNull()) {
+    echo $player->getName();
 
-$allstats = $player->getStats();
-foreach(array_keys($allstats->getRaw()) as $game)
-{
-    echo '<h1>' . $game . '</h1>';
-    echo '<ul>';
-
-    $gamestats = $allstats->getGame($game);
-    foreach($allstats->getGame($game)->getRaw() as $field=>$val)
-    {
-        echo '<li><b>' . $field . ':</b> ' . $val . '</li>';
+    $allstats = $player->getStats();
+    $games = array_keys($allstats->getRaw());
+    foreach ($games as $game) {
+        echo '<h1>' . $game . '</h1>';
+        echo '<ul>';
+        $gamestats = $allstats->getGame($game);
+        foreach ($allstats->getGame($game)->getRaw() as $field => $val) {
+            echo "<li><b>$field:</b> $val</li>";
+        }
+        echo '</ul>';
     }
-    echo '</ul><br />';
-}
 
-echo $player->getStats()->getGame('Paintball')->get('kills', 0);
-echo $player->getStats()->getGame('Paintball')->get('deaths', 0);
+    echo $player->getStats()->getGame('Paintball')->get('kills', 0);
+    echo $player->getStats()->getGame('Paintball')->get('deaths', 0);
+}
