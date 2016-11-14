c739682c8e27ff62804003bf576a2920795f7a78
Plancke <aaronplancker@gmail.com>
2016-11-14 02:06:37 +0100

    change that back

--- src/classes/HypixelObject.php
+++ src/classes/HypixelObject.php
@@ -14,10 +14,11 @@ abstract class HypixelObject extends APIObject {
      * @param HypixelPHP $HypixelPHP
      */
     public function __construct(HypixelPHP $HypixelPHP, $data) {
-        parent::__construct($HypixelPHP, []);
+        parent::__construct($HypixelPHP, $data);
 
-        $this->data['record'] = $data;
-
+        if (!isset($this->data['record']) || !is_array($this->data['record'])) {
+            $this->data['record'] = [];
+        }
         if (!isset($this->data['extra']) || !is_array($this->data['extra'])) {
             $this->data['extra'] = [];
         }
