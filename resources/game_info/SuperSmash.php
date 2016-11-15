91fd079a9449120ae1862dbe1b1f0da55b2a9336
Plancke <aaronplancker@gmail.com>
2016-11-15 05:23:19 +0100

    remove that, useless to anyone but me

--- resources/game_info/SuperSmash.php
+++ resources/game_info/SuperSmash.php
@@ -13,98 +13,66 @@ return array(
         array(
             'THE_BULK' =>
                 array(
-                    'icon' => 'THE_BULK.png',
-                    'masterIcon' => 'THE_BULK_MASTER.png',
                     'display' => 'Bulk',
                 ),
             'GENERAL_CLUCK' =>
                 array(
-                    'icon' => 'GENERAL_CLUCK.png',
-                    'masterIcon' => 'GENERAL_CLUCK_MASTER.png',
                     'display' => 'General Cluck',
                 ),
             'SPODERMAN' =>
                 array(
-                    'icon' => 'SPODERMAN.png',
-                    'masterIcon' => 'SPODERMAN_MASTER.png',
                     'display' => 'Spooderman',
                 ),
             'DUSK_CRAWLER' =>
                 array(
-                    'icon' => 'DUSK_CRAWLER.png',
-                    'masterIcon' => 'DUSK_CRAWLER_MASTER.png',
                     'display' => 'Void Crawler',
                 ),
             'TINMAN' =>
                 array(
-                    'icon' => 'TINMAN.png',
-                    'masterIcon' => 'TINMAN_MASTER.png',
                     'display' => 'Tinman',
                 ),
             'GOKU' =>
                 array(
-                    'icon' => 'GOKU.png',
-                    'masterIcon' => 'GOKU_MASTER.png',
                     'display' => 'Karakot',
                 ),
             'PUG' =>
                 array(
-                    'icon' => 'PUG.png',
-                    'masterIcon' => 'PUG_MASTER.png',
                     'display' => 'Pug',
                 ),
             'FROSTY' =>
                 array(
-                    'icon' => 'FROSTY.png',
-                    'masterIcon' => 'FROSTY_MASTER.png',
                     'display' => 'Cryomancer',
                 ),
             'CAKE_MONSTER' =>
                 array(
-                    'icon' => 'CAKE_MONSTER.png',
-                    'masterIcon' => 'CAKE_MONSTER_MASTER.png',
                     'display' => 'Cake Monster',
                 ),
             'BOTMUN' =>
                 array(
-                    'icon' => 'BOTMUN.png',
-                    'masterIcon' => 'BOTMUN_MASTER.png',
                     'display' => 'Botmon',
                 ),
             'SKULLFIRE' =>
                 array(
-                    'icon' => 'SKULLFIRE.png',
-                    'masterIcon' => 'SKULLFIRE_MASTER.png',
                     'display' => 'Skullfire',
                 ),
             'SHOOP_DA_WHOOP' =>
                 array(
-                    'icon' => 'SHOOP_DA_WHOOP.png',
-                    'masterIcon' => 'SHOOP_DA_WHOOP_MASTER.png',
                     'display' => 'Shoop',
                 ),
             'MARAUDER' =>
                 array(
-                    'icon' => 'MARAUDER.png',
-                    'masterIcon' => 'MARAUDER_MASTER.png',
                     'display' => 'Marauder',
                 ),
             'SERGEANT_SHIELD' =>
                 array(
-                    'icon' => 'SERGEANT_SHIELD.png',
-                    'masterIcon' => 'SERGEANT_SHIELD.png',
                     'display' => 'Sgt. Shield',
                 ),
             'GREEN_HOOD' =>
                 array(
-                    'icon' => 'GREEN_HOOD.png',
-                    'masterIcon' => 'GREEN_HOOD_MASTER.png',
                     'display' => 'Green Hood',
                 ),
             'SANIC' =>
                 array(
-                    'icon' => 'SANIC.png',
-                    'masterIcon' => 'SANIC_MASTER.png',
                     'display' => 'Sanic',
                 ),
         ),
