b66862e2785b1b3a06e4b877c8efa6eb9a0dd921
Plancke <aaronplancker@gmail.com>
2016-12-09 04:38:30 +0100

    remove useless data and make it uniform

--- resources/game_info/tntgames/Wizards.php
+++ resources/game_info/tntgames/Wizards.php
@@ -2,50 +2,28 @@
 return array(
     'classes' =>
         array(
-            'firewizard' =>
+            'list' =>
                 array(
-                    'display' => 'Fire',
-                    'color' =>
+                    'firewizard' =>
                         array(
-                            0 => 'ea8f1a',
-                            1 => 'd46a40',
+                            'display' => 'Fire'
+                        ),
+                    'kineticwizard' =>
+                        array(
+                            'display' => 'Kinetic'
+                        ),
+                    'icewizard' =>
+                        array(
+                            'display' => 'Ice'
+                        ),
+                    'witherwizard' =>
+                        array(
+                            'display' => 'Wither'
+                        ),
+                    'bloodwizard' =>
+                        array(
+                            'display' => 'Blood'
                         ),
                 ),
-            'kineticwizard' =>
-                array(
-                    'display' => 'Kinetic',
-                    'color' =>
-                        array(
-                            0 => '79d976',
-                            1 => '44d741',
-                        ),
-                ),
-            'icewizard' =>
-                array(
-                    'display' => 'Ice',
-                    'color' =>
-                        array(
-                            0 => '9edfe0',
-                            1 => '63a4a5',
-                        ),
-                ),
-            'witherwizard' =>
-                array(
-                    'display' => 'Wither',
-                    'color' =>
-                        array(
-                            0 => 'cb9ee0',
-                            1 => 'bf31d9',
-                        ),
-                ),
-            'bloodwizard' =>
-                array(
-                    'display' => 'Blood',
-                    'color' =>
-                        array(
-                            0 => 'de4d4d',
-                            1 => 'c52424',
-                        ),
-                ),
-        ),
+        )
 );
