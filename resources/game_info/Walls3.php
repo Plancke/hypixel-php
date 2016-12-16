a82713a0966d5195cf80f774b8018e261d0bf563
Plancke <aaronplancker@gmail.com>
2016-12-16 15:47:56 +0100

    implement new walls3 stuff

--- resources/game_info/Walls3.php
+++ resources/game_info/Walls3.php
@@ -2,132 +2,244 @@
 return array(
     'classes' =>
         array(
-            'skeleton' =>
+            'list' =>
                 array(
-                    'display' => 'Skeleton',
-                    'head' => 'MHF_Skeleton',
+                    'HUNTER' =>
+                        array(
+                            'display' => 'Hunter',
+                            'description' => 'Use superior archery skills to pelt enemies with arrows.',
+                            'type' => 'HERO',
+                            'category' => 'RANGED',
+                            'difficulty' => 3,
+                        ),
+                    'ZOMBIE' =>
+                        array(
+                            'display' => 'Zombie',
+                            'description' => 'The Zombie class focuses on defensive gameplay and healing.',
+                            'type' => 'NORMAL',
+                            'category' => 'TANK',
+                            'difficulty' => 2,
+                        ),
+                    'CREEPER' =>
+                        array(
+                            'display' => 'Creeper',
+                            'description' => 'The Creeper class uses explosion based powers to win.',
+                            'type' => 'NORMAL',
+                            'category' => 'DAMAGE',
+                            'difficulty' => 4,
+                        ),
+                    'ARCANIST' =>
+                        array(
+                            'display' => 'Arcanist',
+                            'description' => 'PLACEHOLDER',
+                            'type' => 'HERO',
+                            'category' => 'FIGHTER',
+                            'difficulty' => 1,
+                        ),
+                    'SHAMAN' =>
+                        array(
+                            'display' => 'Shaman',
+                            'description' => 'PLACEHOLDER',
+                            'type' => 'HERO',
+                            'category' => 'FIGHTER',
+                            'difficulty' => 3,
+                        ),
+                    'DREADLORD' =>
+                        array(
+                            'display' => 'Dreadlord',
+                            'description' => 'PLACEHOLDER',
+                            'type' => 'HERO',
+                            'category' => 'RUSHER',
+                            'difficulty' => 1,
+                        ),
+                    'GOLEM' =>
+                        array(
+                            'display' => 'Golem',
+                            'description' => 'PLACEHOLDER',
+                            'type' => 'HERO',
+                            'category' => 'TANK',
+                            'difficulty' => 2,
+                        ),
+                    'SQUID' =>
+                        array(
+                            'display' => 'Squid',
+                            'description' => 'Splashing around, like a Squid, because thats what Squids do. Splash.',
+                            'type' => 'NORMAL',
+                            'category' => 'TANK',
+                            'difficulty' => 1,
+                        ),
+                    'MOLEMAN' =>
+                        array(
+                            'display' => 'Moleman',
+                            'description' => 'Wanna play wacka mole...?',
+                            'type' => 'MYTHIC',
+                            'category' => 'MOBILITY',
+                            'difficulty' => 2,
+                        ),
+                    'ENDERMAN' =>
+                        array(
+                            'display' => 'Enderman',
+                            'description' => 'The Enderman class has special teleportation powers and endurance.',
+                            'type' => 'NORMAL',
+                            'category' => 'MOBILITY',
+                            'difficulty' => 2,
+                        ),
+                    'HEROBRINE' =>
+                        array(
+                            'display' => 'Herobrine',
+                            'description' => 'The Herobrine class uses supernatural abilities to attack and destroy your enemies.',
+                            'type' => 'NORMAL',
+                            'category' => 'FIGHTER',
+                            'difficulty' => 1,
+                        ),
+                    'BLAZE' =>
+                        array(
+                            'display' => 'Blaze',
+                            'description' => 'The Blaze class uses the spirit of fire to enfore flames.',
+                            'type' => 'HERO',
+                            'category' => 'RANGED',
+                            'difficulty' => 2,
+                        ),
+                    'PIGMAN' =>
+                        array(
+                            'display' => 'Pigman',
+                            'description' => 'Half man, half pig, half..oh wait! Feel the power of pork!',
+                            'type' => 'HERO',
+                            'category' => 'FIGHTER',
+                            'difficulty' => 2,
+                        ),
+                    'SPIDER' =>
+                        array(
+                            'display' => 'Spider',
+                            'description' => 'The Spider class uses agile paths for combat.',
+                            'type' => 'NORMAL',
+                            'category' => 'MOBILITY',
+                            'difficulty' => 3,
+                        ),
+                    'WEREWOLF' =>
+                        array(
+                            'display' => 'Werewolf',
+                            'description' => 'It\'s always full moon for this brawler.. Slice and dice your way to victory!',
+                            'type' => 'MYTHIC',
+                            'category' => 'FIGHTER',
+                            'difficulty' => 2,
+                        ),
+                    'PIRATE' =>
+                        array(
+                            'display' => 'Pirate',
+                            'description' => 'Use cunning and wit to survive, or blow enemies up with parrots.',
+                            'type' => 'HERO',
+                            'category' => 'FIGHTER',
+                            'difficulty' => 3,
+                        ),
+                    'PHOENIX' =>
+                        array(
+                            'display' => 'Phoenix',
+                            'description' => 'Born from the ashes of my predecessor I shall erase you from existence!',
+                            'type' => 'MYTHIC',
+                            'category' => 'RANGED',
+                            'difficulty' => 4,
+                        ),
+                    'SKELETON' =>
+                        array(
+                            'display' => 'Skeleton',
+                            'description' => 'The Skeleton class makes excellent use of ranged abilities and weapons.',
+                            'type' => 'NORMAL',
+                            'category' => 'RANGED',
+                            'difficulty' => 3,
+                        ),
                 ),
-            'zombie' =>
+            'cost' =>
                 array(
-                    'display' => 'Zombie',
-                    'head' => 'MHF_Zombie',
+                    'NORMAL' =>
+                        array(
+                            'classes' =>
+                                array(
+                                    0 => 50,
+                                    1 => 125,
+                                    2 => 300,
+                                    3 => 600,
+                                    4 => 3500,
+                                    5 => 6500,
+                                    6 => 8500,
+                                    7 => 14000,
+                                ),
+                            'enderchest' =>
+                                array(
+                                    0 => 3000,
+                                    1 => 7000,
+                                    2 => 14000,
+                                    3 => 70000,
+                                ),
+                            'prestige' =>
+                                array(
+                                    0 => 250000,
+                                    1 => 250000,
+                                    2 => 250000,
+                                ),
+                        ),
+                    'HERO' =>
+                        array(
+                            'classes' =>
+                                array(
+                                    0 => 100,
+                                    1 => 250,
+                                    2 => 600,
+                                    3 => 1200,
+                                    4 => 7000,
+                                    5 => 13000,
+                                    6 => 17000,
+                                    7 => 28000,
+                                ),
+                            'enderchest' =>
+                                array(
+                                    0 => 6000,
+                                    1 => 14000,
+                                    2 => 28000,
+                                    3 => 140000,
+                                ),
+                            'prestige' =>
+                                array(
+                                    0 => 500000,
+                                    1 => 500000,
+                                    2 => 500000,
+                                ),
+                        ),
+                    'MYTHIC' =>
+                        array(
+                            'classes' =>
+                                array(
+                                    0 => 50,
+                                    1 => 125,
+                                    2 => 300,
+                                    3 => 600,
+                                    4 => 3500,
+                                    5 => 6500,
+                                    6 => 8500,
+                                    7 => 14000,
+                                ),
+                            'enderchest' =>
+                                array(
+                                    0 => 3000,
+                                    1 => 7000,
+                                    2 => 14000,
+                                    3 => 70000,
+                                ),
+                            'prestige' =>
+                                array(
+                                    0 => 250000,
+                                    1 => 250000,
+                                    2 => 250000,
+                                ),
+                        ),
                 ),
-            'creeper' =>
+            'fields' =>
                 array(
-                    'display' => 'Creeper',
-                    'head' => 'MHF_Creeper',
+                    0 => 'a',
+                    1 => 'b',
+                    2 => 'c',
+                    3 => 'd',
+                    4 => 'g',
                 ),
-            'enderman' =>
-                array(
-                    'display' => 'Enderman',
-                    'head' => 'MHF_Enderman',
-                ),
-            'herobrine' =>
-                array(
-                    'display' => 'Herobrine',
-                    'head' => 'MHF_Herobrine',
-                ),
-            'spider' =>
-                array(
-                    'display' => 'Spider',
-                    'head' => 'MHF_Spider',
-                ),
-            'squid' =>
-                array(
-                    'display' => 'Squid',
-                    'head' => 'MHF_Squid',
-                ),
-            'dreadlord' =>
-                array(
-                    'display' => 'Dreadlord',
-                    'cost' => 10000,
-                    'hero' => true,
-                    'head' => 'Banald',
-                ),
-            'shaman' =>
-                array(
-                    'display' => 'Shaman',
-                    'cost' => 10000,
-                    'hero' => true,
-                    'head' => 'Zumelos',
-                ),
-            'arcanist' =>
-                array(
-                    'display' => 'Arcanist',
-                    'cost' => 10000,
-                    'hero' => true,
-                    'head' => 'Gonuld',
-                ),
-            'golem' =>
-                array(
-                    'display' => 'Golem',
-                    'cost' => 10000,
-                    'hero' => true,
-                    'head' => 'MHF_Golem',
-                ),
-            'blaze' =>
-                array(
-                    'display' => 'Blaze',
-                    'cost' => 15000,
-                    'hero' => true,
-                    'head' => 'MHF_Blaze',
-                ),
-            'pigman' =>
-                array(
-                    'display' => 'Pigman',
-                    'cost' => 15000,
-                    'hero' => true,
-                    'head' => 'MHF_Pig',
-                ),
-            'pirate' =>
-                array(
-                    'display' => 'Pirate',
-                    'cost' => 20000,
-                    'hero' => true,
-                    'head' => 'samsamsam1234',
-                ),
-            'hunter' =>
-                array(
-                    'display' => 'Hunter',
-                    'cost' => 15000,
-                    'hero' => true,
-                    'head' => '3rdDegreeNinja',
-                ),
-        ),
-    'levels' =>
-        array(
-            'heroMultiplier' => 2,
-            'costs' =>
-                array(
-                    0 => 0,
-                    1 => 100,
-                    2 => 250,
-                    3 => 600,
-                    4 => 1200,
-                    5 => 3500,
-                    6 => 6500,
-                    7 => 8500,
-                    8 => 14000,
-                ),
-        ),
-    'prestige' =>
-        array(
-            'cost' => 250000,
-            'heroMultiplier' => 2,
-            'display' => 'Prestiged',
-        ),
-    'fields' =>
-        array(
-            0 => 'a',
-            1 => 'b',
-            2 => 'c',
-            3 => 'd',
-            4 => 'g',
-        ),
-    'enderchest' =>
-        array(
-            'cost' => 10000,
-            'heroMultiplier' => 1.5,
-            'display' => 'Enderchest',
         ),
 );
