aefe6a1de4f59818ce838190a9bcc4a393c40cc9
Plancke <aaronplancker@gmail.com>
2017-07-04 20:48:45 -0700

    add util method for bedwars

---
+++ src/util/GameUtils.php
@@ -0,0 +1,30 @@
+<?php
+namespace Plancke\HypixelPHP\util;
+
+abstract class GameUtils {
+
+    /**
+     * Calculate level for given bedwars experience
+     *
+     * @param $exp
+     * @return float|int
+     */
+    public static function getBedwarsLevel($exp) {
+        // first few levels are different
+        if ($exp < 500) {
+            return 0;
+        } else if ($exp < 1500) {
+            return 1;
+        } else if ($exp < 3500) {
+            return 2;
+        } else if ($exp < 5500) {
+            return 3;
+        } else if ($exp < 9000) {
+            return 4;
+        }
+
+        $exp -= 9000;
+        return $exp / 5000 + 4;
+    }
+
+}
