c1fc0d910ede47dd723a84cea65dc73cf36ee3f1
Plancke <aaronplancker@gmail.com>
2017-12-16 02:37:44 +0100

    update Bedwars exp calculation

--- src/util/GameUtils.php
+++ src/util/GameUtils.php
@@ -4,6 +4,9 @@ namespace Plancke\HypixelPHP\util;
 
 abstract class GameUtils {
 
+    const BEDWARS_EXP_PER_PRESTIGE = 489000;
+    const BEDWARS_LEVELS_PER_PRESTIGE = 100;
+
     /**
      * Calculate level for given bedwars experience
      *
@@ -11,21 +14,30 @@ abstract class GameUtils {
      * @return float|int
      */
     public static function getBedwarsLevel($exp) {
+        $prestige = (int)($exp / GameUtils::BEDWARS_EXP_PER_PRESTIGE);
+        $exp = $exp % GameUtils::BEDWARS_EXP_PER_PRESTIGE;
+
+        if ($prestige > 5) {
+            $over = $prestige % 5;
+            $exp += $over * GameUtils::BEDWARS_EXP_PER_PRESTIGE;
+            $prestige -= $over;
+        }
+
         // first few levels are different
         if ($exp < 500) {
-            return 0;
+            return 0 + ($prestige * GameUtils::BEDWARS_LEVELS_PER_PRESTIGE);
         } else if ($exp < 1500) {
-            return 1;
+            return 1 + ($prestige * GameUtils::BEDWARS_LEVELS_PER_PRESTIGE);
         } else if ($exp < 3500) {
-            return 2;
+            return 2 + ($prestige * GameUtils::BEDWARS_LEVELS_PER_PRESTIGE);
         } else if ($exp < 5500) {
-            return 3;
+            return 3 + ($prestige * GameUtils::BEDWARS_LEVELS_PER_PRESTIGE);
         } else if ($exp < 9000) {
-            return 4;
+            return 4 + ($prestige * GameUtils::BEDWARS_LEVELS_PER_PRESTIGE);
         }
 
         $exp -= 9000;
-        return $exp / 5000 + 4;
+        return ($exp / 5000 + 4) + ($prestige * GameUtils::BEDWARS_LEVELS_PER_PRESTIGE);
     }
 
 }
