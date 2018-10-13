7dda34f448c602ccc016d13c3a366cf3a2cdb2e0
Plancke <aaronplancker@gmail.com>
2018-10-14 00:02:46 +0200

    bedwars level fix?

--- src/util/games/BedwarsUtils.php
+++ src/util/games/BedwarsUtils.php
@@ -8,8 +8,49 @@ namespace Plancke\HypixelPHP\util\games;
  */
 class BedwarsUtils {
 
-    const BEDWARS_EXP_PER_PRESTIGE = 489000;
-    const BEDWARS_LEVELS_PER_PRESTIGE = 100;
+    const EASY_LEVELS = 4;
+    const EASY_LEVELS_XP = 7000;
+    const XP_PER_PRESTIGE = 96 * 5000 + BedwarsUtils::EASY_LEVELS_XP;
+    const LEVELS_PER_PRESTIGE = 100;
+    const HIGHEST_PRESTIGE = 10;
+
+    public function getExpForLevel($level) {
+        if ($level == 0) return 0;
+
+        $respectedLevel = $this->getLevelRespectingPrestige($level);
+        if ($respectedLevel > BedwarsUtils::EASY_LEVELS) {
+            return 5000;
+        }
+
+        switch ($respectedLevel) {
+            case 1:
+                return 500;
+            case 2:
+                return 1000;
+            case 3:
+                return 2000;
+            case 4:
+                return 3500;
+        }
+        return 5000;
+    }
+
+    public function getLevelForExpRespectingPrestige($exp) {
+        return $this->getLevelForExp($exp);
+    }
+
+    /**
+     * Returns "2" instead of "102" if prestiges happen every 100 levels e.g.
+     * @param $level
+     * @return float|int
+     */
+    public function getLevelRespectingPrestige($level) {
+        if ($level > BedwarsUtils::HIGHEST_PRESTIGE * BedwarsUtils::LEVELS_PER_PRESTIGE) {
+            return $level - BedwarsUtils::HIGHEST_PRESTIGE * BedwarsUtils::LEVELS_PER_PRESTIGE;
+        } else {
+            return $level % BedwarsUtils::LEVELS_PER_PRESTIGE;
+        }
+    }
 
     /**
      * Calculate level for given bedwars experience
@@ -17,31 +58,23 @@ class BedwarsUtils {
      * @param $exp
      * @return float|int
      */
-    public function getBedwarsLevel($exp) {
-        $prestige = (int)($exp / BedWarsUtils::BEDWARS_EXP_PER_PRESTIGE);
-        $exp = $exp % BedWarsUtils::BEDWARS_EXP_PER_PRESTIGE;
+    public function getLevelForExp($exp) {
+        $prestiges = $exp / BedwarsUtils::XP_PER_PRESTIGE;
 
-        if ($prestige > 5) {
-            $over = $prestige % 5;
-            $exp += $over * BedWarsUtils::BEDWARS_EXP_PER_PRESTIGE;
-            $prestige -= $over;
+        $level = $prestiges * BedwarsUtils::LEVELS_PER_PRESTIGE;
+
+        $expWithoutPrestiges = $exp - ($prestiges * BedwarsUtils::XP_PER_PRESTIGE);
+        for ($i = 1; $i <= BedwarsUtils::EASY_LEVELS; ++$i) {
+            $expForEasyLevel = $this->getExpForLevel($i);
+            if ($expWithoutPrestiges < $expForEasyLevel) {
+                break;
+            }
+            $level++;
+            $expWithoutPrestiges -= $expForEasyLevel;
         }
+        $level += $expWithoutPrestiges / 5000;
 
-        // first few levels are different
-        if ($exp < 500) {
-            return 0 + ($prestige * BedWarsUtils::BEDWARS_LEVELS_PER_PRESTIGE);
-        } else if ($exp < 1500) {
-            return 1 + ($prestige * BedWarsUtils::BEDWARS_LEVELS_PER_PRESTIGE);
-        } else if ($exp < 3500) {
-            return 2 + ($prestige * BedWarsUtils::BEDWARS_LEVELS_PER_PRESTIGE);
-        } else if ($exp < 5500) {
-            return 3 + ($prestige * BedWarsUtils::BEDWARS_LEVELS_PER_PRESTIGE);
-        } else if ($exp < 9000) {
-            return 4 + ($prestige * BedWarsUtils::BEDWARS_LEVELS_PER_PRESTIGE);
-        }
-
-        $exp -= 9000;
-        return ($exp / 5000 + 4) + ($prestige * BedWarsUtils::BEDWARS_LEVELS_PER_PRESTIGE);
+        return $level;
     }
 
 }
