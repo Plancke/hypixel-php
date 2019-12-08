726d945b679323d77f56c2ac831f5103ced1d5f2
Plancke <aaronplancker@gmail.com>
2019-12-08 21:33:13 +0100

    cleanup nocachehandler

--- src/cache/impl/NoCacheHandler.php
+++ src/cache/impl/NoCacheHandler.php
@@ -3,7 +3,6 @@
 namespace Plancke\HypixelPHP\cache\impl;
 
 use Plancke\HypixelPHP\cache\CacheHandler;
-use Plancke\HypixelPHP\fetch\Response;
 use Plancke\HypixelPHP\responses\booster\Boosters;
 use Plancke\HypixelPHP\responses\friend\Friends;
 use Plancke\HypixelPHP\responses\gameCounts\GameCounts;
@@ -13,6 +12,7 @@ use Plancke\HypixelPHP\responses\Leaderboards;
 use Plancke\HypixelPHP\responses\player\Player;
 use Plancke\HypixelPHP\responses\PlayerCount;
 use Plancke\HypixelPHP\responses\Session;
+use Plancke\HypixelPHP\responses\skyblock\SkyBlockAuctions;
 use Plancke\HypixelPHP\responses\skyblock\SkyBlockProfile;
 use Plancke\HypixelPHP\responses\WatchdogStats;
 
@@ -26,226 +26,108 @@ use Plancke\HypixelPHP\responses\WatchdogStats;
  */
 class NoCacheHandler extends CacheHandler {
 
-    /**
-     * @param Player $player
-     * @return void
-     */
     function setPlayer(Player $player) {
     }
 
-    /**
-     * @param $uuid
-     * @return null|Response|Player
-     */
     function getPlayer($uuid) {
         return null;
     }
 
-    /**
-     * @param $username
-     * @param $uuid
-     * @return void
-     */
     function setPlayerUUID($username, $uuid) {
     }
 
-    /**
-     * @param $username
-     * @return string
-     */
     function getUUID($username) {
         return null;
     }
 
-    /**
-     * @param Guild $guild
-     * @return void
-     */
     function setGuild(Guild $guild) {
     }
 
-    /**
-     * @param $id
-     * @return null|Response|Guild
-     */
     function getGuild($id) {
         return null;
     }
 
-    /**
-     * @param $uuid
-     * @param $id
-     * @return void
-     */
     function setGuildIDForUUID($uuid, $id) {
     }
 
-    /**
-     * @param $uuid
-     * @return mixed
-     */
     function getGuildIDForUUID($uuid) {
         return null;
     }
 
-    /**
-     * @param $name
-     * @param $id
-     * @return void
-     */
     function setGuildIDForName($name, $id) {
     }
 
-    /**
-     * @param $name
-     * @return mixed
-     */
     function getGuildIDForName($name) {
         return null;
     }
 
-    /**
-     * @param Friends $friends
-     * @return void
-     */
     function setFriends(Friends $friends) {
     }
 
-    /**
-     * @param $uuid
-     * @return null|Response|Friends
-     */
     function getFriends($uuid) {
         return null;
     }
 
-    /**
-     * @param Session $session
-     * @return void
-     */
     function setSession(Session $session) {
     }
 
-    /**
-     * @param $uuid
-     * @return null|Response|Session
-     */
     function getSession($uuid) {
         return null;
     }
 
-    /**
-     * @param KeyInfo $keyInfo
-     * @return void
-     */
     function setKeyInfo(KeyInfo $keyInfo) {
     }
 
-    /**
-     * @param $key
-     * @return null|Response|KeyInfo
-     */
     function getKeyInfo($key) {
         return null;
     }
 
-    /**
-     * @param Leaderboards $leaderboards
-     * @return void
-     */
     function setLeaderboards(Leaderboards $leaderboards) {
     }
 
-    /**
-     * @return null|Response|Leaderboards
-     */
     function getLeaderboards() {
         return null;
     }
 
-    /**
-     * @param Boosters $boosters
-     * @return void
-     */
     function setBoosters(Boosters $boosters) {
     }
 
-    /**
-     * @return null|Response|Boosters
-     */
     function getBoosters() {
         return null;
     }
 
-    /**
-     * @param WatchdogStats $watchdogStats
-     * @return void
-     */
     function setWatchdogStats(WatchdogStats $watchdogStats) {
     }
 
-    /**
-     * @return null|Response|WatchdogStats
-     */
     function getWatchdogStats() {
         return null;
     }
 
-    /**
-     * @param PlayerCount $playerCount
-     * @return void
-     */
     function setPlayerCount(PlayerCount $playerCount) {
     }
 
-    /**
-     * @return null|Response|PlayerCount
-     */
     function getPlayerCount() {
         return null;
     }
 
-    /**
-     * @param GameCounts $gameCounts
-     * @return void
-     */
     function setGameCounts(GameCounts $gameCounts) {
     }
 
-    /**
-     * @return null|Response|GameCounts
-     */
     function getGameCounts() {
         return null;
     }
 
-    /**
-     * @param $profile_id
-     * @return SkyBlockProfile|null
-     */
     public function getSkyBlockProfile($profile_id) {
         return null;
     }
 
-    /**
-     * @param SkyBlockProfile $profile
-     * @return void
-     */
     public function setSkyBlockProfile(SkyBlockProfile $profile) {
     }
 
-    /**
-     * @param $resource
-     * @return Resource
-     */
     public function getResource($resource) {
         return null;
     }
 
-    /**
-     * @param Resource $resource
-     * @return void
-     */
     public function setResource($resource) {
     }
 }
