5d5c28dc19c76ad3ff2efe60eab9681a77450902
Plancke <aaronplancker@gmail.com>
2022-11-02 19:24:37 +0100

    gametypes don't need the hasBoosters

--- src/classes/serverType/GameType.php
+++ src/classes/serverType/GameType.php
@@ -7,27 +7,4 @@ namespace Plancke\HypixelPHP\classes\serverType;
  * @package Plancke\HypixelPHP\classes\serverType
  */
 class GameType extends ServerType {
-
-    protected $boosters;
-
-    /**
-     * @param string $enum
-     * @param string $db
-     * @param string $name
-     * @param string $short
-     * @param int $id
-     * @param bool $boosters
-     */
-    public function __construct($enum, $db, $name, $short, $id, $boosters = true) {
-        parent::__construct($enum, $db, $name, $short, $id);
-        $this->boosters = $boosters;
-    }
-
-    /**
-     * @return boolean
-     */
-    public function hasBoosters() {
-        return $this->boosters;
-    }
-
 }
